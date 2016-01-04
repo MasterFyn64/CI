f2077de6e8f930e3b373bf417a0cca9b90d4e7a3
Francisco Barros <masterk6418@gmail.com>
2016-01-04 16:41:45 +0000

    fix

--- application/views/appointments.php
+++ application/views/appointments.php
@@ -35,10 +35,7 @@
 
                 <?php
             }
-            else
-            {
-                echo "ERRRRORRRER";
-            }
+
             ?>
             <button type="button" class="btn btn-primary" data-toggle = "modal" data-target = "#appointments">New Appointment</button><br/><br/>
 
